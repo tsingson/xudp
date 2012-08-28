384ed46ca331435d2fc02bcbbb8db36286d5839c
jimt <jimteeuwen@gmail.com>
2012-08-28 18:58:02 +0200

    Fixes over-zealous find/replace action.

--- connection.go
+++ connection.go
@@ -11,9 +11,9 @@ import (
 )
 
 var (
-	ErrSocketOpen   = errors.New("Socket is already open.")
-	ErrSocketClosed = errors.New("Socket is already closed.")
-	ErrSendFailed   = errors.New("Send was incomplete.")
+	ErrConnectionOpen   = errors.New("Connection is already open.")
+	ErrConnectionClosed = errors.New("Connection is already closed.")
+	ErrSendFailed       = errors.New("Send was incomplete.")
 )
 
 // A connection allows two-way communication with an end point.
@@ -22,15 +22,15 @@ var (
 type Connection struct {
 	buf     Packet         // Temporary receive buffer.
 	udp     net.PacketConn // Sockets underlying connection.
-	proto   uint32         // Protocol ID identifying our packetc.
+	proto   uint32         // Protocol ID identifying our packets.
 	mtu     uint32         // Maximum transport unit.
-	Timeout uint           // Timeout defines the connection timeout in secondc.
+	Timeout uint           // Timeout defines the connection timeout in seconds.
 }
 
 // NewConnection creates a new connection.
 //
-// MTU defines the maximum size of a single packet in bytec.
-// This includes the UDP and XUDP headerc.
+// MTU defines the maximum size of a single packet in bytes.
+// This includes the UDP and XUDP headers.
 // The available payload space can be calculated as:
 //
 //     payloadSize := MTU - UDPHeaderSize - XUDPHeaderSize
@@ -38,21 +38,21 @@ type Connection struct {
 // Some commonly used values are as follows:
 //
 //     1500 - The largest Ethernet packet size. This is the typical setting for
-//            non-PPPoE, non-VPN connectionc. The default value for NETGEAR
-//            routers, adapters and switchec.
-//     1492 - The size PPPoE preferc.
+//            non-PPPoE, non-VPN connections. The default value for NETGEAR
+//            routers, adapters and switches.
+//     1492 - The size PPPoE prefers.
 //     1472 - Maximum size to use for pinging (Bigger packets are fragmented).
-//     1468 - The size DHCP preferc.
+//     1468 - The size DHCP prefers.
 //     1460 - Usable by AOL if you don't have large email attachments, etc.
 //     1430 - The size VPN and PPTP prefer.
 //     1400 - Maximum size for AOL DSL.
-//      576 - Typical value to connect to dial-up ISPc.
+//      576 - Typical value to connect to dial-up ISPs.
 //
 // The protocol Id is a numerical identifier for all the packets
 // sent and received by our program. It can be any number we want, but
 // it is advised to use something relatively unique.
 // If an incoming packet does not start with this number, discard it
-// because it is not meant for uc. A 4 byte hash of the name of your
+// because it is not meant for us. A 4 byte hash of the name of your
 // program can be a suitable protocol Id.
 func NewConnection(mtu, protocolId uint32) *Connection {
 	c := new(Connection)
@@ -66,7 +66,7 @@ func NewConnection(mtu, protocolId uint32) *Connection {
 // Open opens the connection on the given port number.
 func (c *Connection) Open(port int) (err error) {
 	if c.udp != nil {
-		return ErrSocketOpen
+		return ErrConnectionOpen
 	}
 
 	c.udp, err = net.ListenPacket("udp", fmt.Sprintf(":%d", port))
@@ -84,7 +84,7 @@ func (c *Connection) Open(port int) (err error) {
 // Close closes the connection.
 func (c *Connection) Close() (err error) {
 	if c.udp == nil {
-		return ErrSocketClosed
+		return ErrConnectionClosed
 	}
 
 	err = c.udp.Close()
@@ -95,7 +95,7 @@ func (c *Connection) Close() (err error) {
 // Send sends the given packet to the specified destination.
 func (c *Connection) Send(addr net.Addr, packet Packet) (err error) {
 	if c.udp == nil {
-		return ErrSocketClosed
+		return ErrConnectionClosed
 	}
 
 	packet.SetProtocol(c.proto)
@@ -120,7 +120,7 @@ func (c *Connection) Send(addr net.Addr, packet Packet) (err error) {
 // Recv receives a new packet. This is a blocking operation.
 func (c *Connection) Recv() (addr net.Addr, packet Packet, err error) {
 	if c.udp == nil {
-		return nil, nil, ErrSocketClosed
+		return nil, nil, ErrConnectionClosed
 	}
 
 	size, addr, err := c.udp.ReadFrom(c.buf)
@@ -129,7 +129,7 @@ func (c *Connection) Recv() (addr net.Addr, packet Packet, err error) {
 	}
 
 	if size < XUDPHeaderSize || c.buf.Protocol() != c.proto {
-		return // Not enough data or not meant for uc.
+		return // Not enough data or not meant for us.
 	}
 
 	packet = make(Packet, size)
