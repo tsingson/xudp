57b7d0d3848f3ef503182c92ec2116e6798b7e25
jimt <jimteeuwen@gmail.com>
2012-08-30 15:02:07 +0200

    Adds some more info to README and doc.go.

--- doc.go
+++ doc.go
@@ -14,7 +14,7 @@ Features include:
 	* IPv4 and IPv6 support.
 	* Highly redundant reception acknowledgement by piggybacking multiple
 	  ACKs on regular data packets.
-	* Expose event handlers for cases where indivual packets are lost or ACK'ed.
+	* Exposes event handlers for cases where indivual packets are lost or ACK'ed.
 	  This allows the host application to implement resending of lost packets.
 	  Our library does not do this for performance reasons. To be more
 	  precise: when TCP detects a packet loss, it stops the sending of
@@ -24,5 +24,7 @@ Features include:
 	  We therefor leave it to the host application to determine
 	  what to do when packets are lost. It can resend packets selectively
 	  while not preventing the reception of remaining data.
+	* This packet explicitely does **not** guarantee in-order reception
+	  of packets, for the same reason described in the previous point.
 */
 package xudp
